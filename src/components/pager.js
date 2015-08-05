b158be33b76baff2eb91710e6992de7ba544f2a0
Edward Paget <edward@cassetteta.pe>
2015-08-05 14:16:40 -0500

    Finish and test basic pager class

--- src/components/pager.js
+++ src/components/pager.js
@@ -1,31 +1,69 @@
 import React from 'react';
 
+let NOOP = Function.prototype;
 
 export default React.createClass({
     displayName: 'Pager',
 
     getDefaultProps() {
         return {
-            currentPage: 1
+            currentPage: 1,
+            resourceProp: "resource",
+            onPageChange: NOOP,
+            getPage: (page) => { return Promise.resolve({}); }
         };
     },
 
     getInitialState() {
         return {
-            data: [1, 2, 3]
+            nextPage: 0,
+            previousPage: 0,
+            firstPage: 0,
+            currentPage: null,
+            lastPage: 0,
+            data: []
         };
     },
 
-    render() {
+    getPage(page) {
+        return this.props.getPage(page)
+            .then(({currentPage, nextPage, lastPage, firstPage, previousPage, data}) => {
+                this.state.data[currentPage - 1] = data;
+                data = this.state.data;
+                this.setState({currentPage, nextPage, lastPage, firstPage, previousPage, data}, () => {
+                    this.props.onPageChange(this.state.currentPage);
+                });
+            });
+    },
+
+    componentWillMount() {
+        this.getPage(this.state.currentPage || this.props.currentPage);
+    },
+
+    children(dataPage) {
         let childTemplate = React.Children.only(this.props.children);
-        let children = this.state.data.map((datum, idx) => {
-            let newProps = Object.assign({}, childTemplate.props, {resource: datum, key: idx});
+        return dataPage.map((datum, idx) => {
+            let newProps = Object.assign({}, childTemplate.props, {key: idx});
+            newProps[this.props.resourceProp] = datum;
             return React.createElement(childTemplate.type, newProps);
         });
+    },
+
+    renderPage(page, idx) {
+        return (
+            <div key={idx} className="page">
+                {this.children(page)}
+            </div>
+        );
+    },
+
+    render() {
         return(
-                <div className="paged">
-                    {children}
+            <div className="pager">
+                <div className="paged-resources">
+                    {this.state.data.map(this.renderPage)}
                 </div>
+            </div>
         );
     }
 });
--- test/components/pager-test.js
+++ test/components/pager-test.js
@@ -2,7 +2,10 @@ import dom from '../test-setup';
 
 import React, {addons} from 'react/addons';
 import chai from 'chai';
-let {expect} = chai;
+import spies from 'chai-spies';
+
+chai.use(spies);
+let {expect, spy} = chai;
 
 import Pager from '../../src/components/pager';
 
@@ -21,28 +24,121 @@ var Child = React.createClass({
 });
 
 describe("Pager", () => {
+    var pager;
+
     it("should exist", () => {
         expect(Pager).to.be.ok;
     });
 
+
+    beforeEach(() => {
+        let pageFn = (page) => {
+            let data = [[1,2,3], [4,5,6]];
+            return Promise.resolve({
+                currentPage: page,
+                nextPage: page == 2 ? null : 2,
+                lastPage: 2,
+                firstPage: 1,
+                previousPage: page == 1 ? null : 1 ,
+                data: data[page - 1]
+            });
+        };
+        pager = addons.TestUtils.renderIntoDocument(<Pager getPage={pageFn}>
+                                                        <Child otherProp={false} />
+                                                    </Pager>);
+    });
+
+    describe("#getPage", () => {
+        it("should call the onPageChange callback", () => {
+            let opcCbSpy = spy();
+            pager = addons.TestUtils.renderIntoDocument(<Pager onPageChange={opcCbSpy}>
+                                                            <Child otherProp={false} />
+                                                        </Pager>);
+            return pager.getPage(2).then( () => {
+                expect(opcCbSpy).to.have.been.called.twice();
+            });
+        });
+
+        it("should update the pager state", () => {
+            return pager.getPage(2).then(() => {
+                expect(pager.state).to.deep.equal({
+                    currentPage: 2,
+                    data: [[1,2,3],[4,5,6]],
+                    firstPage: 1,
+                    lastPage: 2,
+                    previousPage: 1,
+                    nextPage: null
+                });
+            });
+        });
+    });
+
+    describe("#componentWillMount", () => {
+        var getPageSpy;
+
+        beforeEach(() => {
+            getPageSpy = spy.on(pager, 'getPage');
+        });
+
+        context("when state.currentPage is set", () => {
+            it("should call this.getPage with state.currentPage", () => {
+                pager.state.currentPage = 5;
+                pager.componentWillMount();
+                expect(getPageSpy).to.have.been.called.with(5);
+            });
+        });
+
+        context("when props.currentPage is set", () => {
+            it("should call this.getPage with props.currentPage", () => {
+                pager.state.currentPage = null;
+                pager.componentWillMount();
+                expect(getPageSpy).to.have.been.called.with(1);
+            });
+        });
+    });
+
+    describe("#children", () => {
+        var childrens;
+
+        beforeEach(() => {
+            childrens = pager.children([1,2,3,4]);
+        });
+
+        it("should return children equal to the page size", () => {
+            expect(childrens).to.have.length(4);
+        });
+
+        it("should return children with a resource prop", () => {
+            childrens.forEach((childs) => {
+                expect(childs.props).to.have.property('resource');
+            });
+        });
+
+        it("should preserve original properties of the child", () => {
+            childrens.forEach((childs) => {
+                expect(childs.props).to.have.property("testProp");
+                expect(childs.props).to.have.property("otherProp");
+            });
+        });
+    });
+
     describe("#render", () => {
-        var pager, child;
+        var firstChild;
 
         beforeEach( () => {
-            pager = addons.TestUtils.renderIntoDocument(<Pager><Child otherProp={false} /></Pager>);
-            ([child] = addons.TestUtils.scryRenderedDOMComponentsWithTag(pager, 'p'));
+           ([firstChild] = addons.TestUtils.scryRenderedDOMComponentsWithTag(pager, 'p'));
         });
 
         it("should apply data to each child", () => {
-            expect(child.getDOMNode().textContent).to.eq('1');
+            expect(firstChild.getDOMNode().textContent).to.eq('1');
         });
 
         it("should preserve the child element's default Props", () => {
-            expect(child.props).to.have.property("testProp").that.is.true;
+            expect(firstChild.props).to.have.property("testProp").that.is.true;
         });
 
         it("should preserve props passed to the child", () => {
-            expect(child.props).to.have.property("otherProp").that.is.false;
+            expect(firstChild.props).to.have.property("otherProp").that.is.false;
         });
     });
 });
